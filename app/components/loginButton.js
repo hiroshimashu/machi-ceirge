73bd16644e1fdf15c2b978cbe6007e41ecc35f62
hiroshimashu <hiroshimashu@gmail.com>
2019-01-13 23:23:11 +0900

    first commit

---
+++ app/components/loginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+
+const onPress = () => {
+ console.log("button pressed");
+}
+const LoginButton = () => {
+  return <Button title="Facebook" />;
+};
+
+export default LoginButton;
