8ebd1f89db966e3f6a136c0f00387a398d2a8548
hiroshimashu <hiroshimashu@gmail.com>
2019-01-25 16:24:52 +0900

    add Search button to header

--- app/App.js
+++ app/App.js
@@ -6,6 +6,7 @@ import {
   createAppContainer,
 } from 'react-navigation';
 import thunk from 'redux-thunk';
+import { SearchBar } from 'react-native-elements';
 import AuthScreen from './screens/AuthScreen';
 import FindScreen from './screens/FindScreen';
 import GoodScreen from './screens/GoodScreen';
@@ -13,15 +14,33 @@ import MyProfileSceen from './screens/MyProfileScreen';
 import TalkScreen from './screens/TalkScreen';
 import store from './store';
 
+// To do: Make TabScreen unswipable to AuthScreen.
 export default class App extends React.Component {
   render() {
+    const navigationOptions = ({ navigation }) => {
+      return {
+        tabBarVisible: false,
+        swipeEnabled: false,
+        headerLeftContainerStyle: { flex:1 },
+        headerLeft: (
+          <SearchBar
+            editable={false}
+            icon={{ type: 'font-awesome', name: 'search' }}
+            placeholder="検索条件を設定する"
+            round
+            containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, height: 25, top: -5 }}
+            inputStyle = {{ top: -5 }}
+          />
+        ),
+      };
+    };
+
     const TabScreen = createBottomTabNavigator({
       Find: FindScreen,
       Good: GoodScreen,
       Talk: TalkScreen,
       MyProfile: MyProfileSceen,
     });
-
     const MainNavigator = createStackNavigator({
       Auth: {
         screen: AuthScreen,
@@ -31,10 +50,7 @@ export default class App extends React.Component {
       },
       Tab: {
         screen: TabScreen,
-        navigationOptions: {
-          tabBarVisible: false,
-          headerLeft: null,
-        },
+        navigationOptions,
       },
     });
     const MainScreen = createAppContainer(MainNavigator);
